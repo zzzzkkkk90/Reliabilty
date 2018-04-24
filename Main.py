dfd913d74989e5ba948ee05c6c1fa53fb2395fe1
frankstyle520 <frankstyle520@users.noreply.github.com>
2018-04-24 18:16:41 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -37,7 +37,7 @@ def bootstrap():
     count = 0
     for dataset, filename in Processing().import_single_data():
         print ('filename', filename)
-        count += 1
+
         training_data_X, training_data_y, testing_data_X, testing_data_y = Processing(
         ).separate_data(dataset)
 
@@ -48,10 +48,12 @@ def bootstrap():
         r = r.tolist()
         u, n = PerformanceMeasure(training_data_y).calc_UN(type='cs')
 
+        count += 1
         global Loss
         csga = pyGaft(objfunc=Loss, var_bounds=[(-1, 1)] * 20,
                       individual_size=100, max_iter=20, max_or_min='min',
                       P=P, r=r, u=u, n=n).run()
+
         if count == 1:
             import best_fit
         else:
@@ -61,11 +63,13 @@ def bootstrap():
         print('w = ', w)
         csrs_pred_y = RankSVM(w=w).predict3(testing_data_X)
         csrs_fpa = PerformanceMeasure(testing_data_y, csrs_pred_y).FPA()
-        print('csrs_fpa:', csrs_fpa)
+        # print('csrs_fpa:', csrs_fpa)
+        csrs_fpa_list.append(csrs_fpa)
 
         # IR SVM
         u, n = PerformanceMeasure(training_data_y).calc_UN(type='ir')
 
+        count += 1
         irga = pyGaft(objfunc=Loss, var_bounds=[(-1, 1)] * 20,
                       individual_size=100, max_iter=20, max_or_min='min',
                       P=P, r=r, u=u, n=n).run()
@@ -78,7 +82,8 @@ def bootstrap():
         print('w = ', w)
         irsvm_pred_y = RankSVM(w=w).predict3(testing_data_X)
         irsvm_fpa = PerformanceMeasure(testing_data_y, irsvm_pred_y).FPA()
-        print('irsvm_fpa:', irsvm_fpa)
+        # print('irsvm_fpa:', irsvm_fpa)
+        irsvm_fpa_list.append(irsvm_fpa)
 
         # 这个是LTR
         training_datalist_X = training_data_X.tolist()
@@ -86,6 +91,7 @@ def bootstrap():
 
         from PyOptimize.General_Opt import Test_function
 
+        count += 1
         global LTR
         ltrga = pyGaft(objfunc=LTR, var_bounds=[(-2, 2)] * 20,
                        individual_size=50, max_iter=10, max_or_min='max',
@@ -102,7 +108,8 @@ def bootstrap():
         for test_x in testing_data_X:
             ltr_pred_y.append(np.dot(test_x, a))
         ltr_fpa = PerformanceMeasure(testing_data_y, ltr_pred_y).FPA()
-        print('ltr_fpa', ltr_fpa)
+        # print('ltr_fpa', ltr_fpa)
+        ltr_fpa_list.append(ltr_fpa)
 
         # 在原始数据集上训练Ranking SVM,DTR,LR,BRR
 
@@ -111,22 +118,26 @@ def bootstrap():
         rs = RankSVM(C=1.0).fit(shuf_X, shuf_y)
         rs_pred_y = np.around(rs.predict2(testing_data_X))
         rs_fpa = PerformanceMeasure(testing_data_y, rs_pred_y).FPA()
-        print('rs_fpa:', rs_fpa)
+        # print('rs_fpa:', rs_fpa)
+        rs_fpa_list.append(rs_fpa)
 
         dtr = DecisionTreeRegressor().fit(training_data_X, training_data_y)
         dtr_pred_y = dtr.predict(testing_data_X)
         dtr_fpa = PerformanceMeasure(testing_data_y, dtr_pred_y).FPA()
-        print('dtr_fpa:', dtr_fpa)
+        # print('dtr_fpa:', dtr_fpa)
+        dtr_fpa_list.append(dtr_fpa)
 
         lr = linear_model.LinearRegression().fit(training_data_X, training_data_y)
         lr_pred_y = lr.predict(testing_data_X)
         lr_fpa = PerformanceMeasure(testing_data_y, lr_pred_y).FPA()
-        print('lr_fpa:', lr_fpa)
+        # print('lr_fpa:', lr_fpa)
+        lr_fpa_list.append(lr_fpa)
 
         brr = BayesianRidge().fit(training_data_X, training_data_y)
         brr_pred_y = brr.predict(testing_data_X)
         brr_fpa = PerformanceMeasure(testing_data_y, brr_pred_y).FPA()
-        print('brr_fpa:', brr_fpa)
+        # print('brr_fpa:', brr_fpa)
+        brr_fpa_list.append(brr_fpa)
 
         # 先对训练数据集进行RUS处理，然后训练Ranking SVM, DTR,LR,BRR
         rus_X, rus_y, _id = RandomUnderSampler(
@@ -137,7 +148,7 @@ def bootstrap():
         training_datalist_y = rus_y.tolist()
 
         from PyOptimize.General_Opt import Test_function
-
+        count += 1
         rus_ltrga = pyGaft(objfunc=LTR, var_bounds=[(-2, 2)] * 20,
                            individual_size=50, max_iter=10, max_or_min='max',
                            X=training_datalist_X, y=training_datalist_y).run()
@@ -153,29 +164,72 @@ def bootstrap():
         for test_x in testing_data_X:
             rus_ltr_pred_y.append(np.dot(test_x, rus_a))
         rus_ltr_fpa = PerformanceMeasure(testing_data_y, rus_ltr_pred_y).FPA()
-        print('rus_ltr_fpa', rus_ltr_fpa)
+        # print('rus_ltr_fpa', rus_ltr_fpa)
+        rus_ltr_fpa_list.append(rus_ltr_fpa)
 
         shuf_X, shuf_y = shuffle(rus_X, rus_y)
         rus_rs = RankSVM(C=1.0).fit(shuf_X, shuf_y)
         rus_rs_pred_y = rus_rs.predict2(testing_data_X)
         rus_rs_fpa = PerformanceMeasure(testing_data_y, rus_rs_pred_y).FPA()
-        print('rus_rs_fpa:', rus_rs_fpa)
+        # print('rus_rs_fpa:', rus_rs_fpa)
+        rus_rs_fpa_list.append(rus_rs_fpa)
 
         rus_dtr = DecisionTreeRegressor().fit(rus_X, rus_y)
         rus_dtr_pred_y = rus_dtr.predict(testing_data_X)
         rus_dtr_fpa = PerformanceMeasure(testing_data_y, rus_dtr_pred_y).FPA()
-        print('rus_dtr_fpa:', rus_dtr_fpa)
+        # print('rus_dtr_fpa:', rus_dtr_fpa)
+        rus_dtr_fpa_list.append(rus_dtr_fpa)
 
         rus_lr = linear_model.LinearRegression().fit(rus_X, rus_y)
         rus_lr_pred_y = rus_lr.predict(testing_data_X)
         rus_lr_fpa = PerformanceMeasure(testing_data_y, rus_lr_pred_y).FPA()
-        print('rus_lr_fpa:', rus_lr_fpa)
+        # print('rus_lr_fpa:', rus_lr_fpa)
+        rus_lr_fpa_list.append(rus_lr_fpa)
 
         rus_brr = BayesianRidge().fit(rus_X, rus_y)
         rus_brr_pred_y = rus_brr.predict(testing_data_X)
         rus_brr_fpa = PerformanceMeasure(testing_data_y, rus_brr_pred_y).FPA()
-        print('rus_brr_fpa:', rus_brr_fpa)
+        # print('rus_brr_fpa:', rus_brr_fpa)
+        rus_brr_fpa_list.append(rus_brr_fpa)
 
 
 if __name__ == '__main__':
-    bootstrap()
+    csrs_fpa_list = []
+    irsvm_fpa_list = []
+    ltr_fpa_list = []
+    rs_fpa_list = []
+    dtr_fpa_list = []
+    lr_fpa_list = []
+    brr_fpa_list = []
+    rus_ltr_fpa_list = []
+    rus_rs_fpa_list = []
+    rus_dtr_fpa_list = []
+    rus_lr_fpa_list = []
+    rus_brr_fpa_list = []
+    for _ in range(1):
+        bootstrap()
+    csrs_fpa_average = sum(csrs_fpa_list) / 50
+    irsvm_fpa_average = sum(irsvm_fpa_list) / 50
+    ltr_fpa_average = sum(ltr_fpa_list) / 50
+    rs_fpa_average = sum(rs_fpa_list) / 50
+    dtr_fpa_average = sum(dtr_fpa_list) / 50
+    lr_fpa_average = sum(lr_fpa_list) / 50
+    brr_fpa_average = sum(brr_fpa_list) / 50
+    rus_ltr_fpa_average = sum(rus_ltr_fpa_list) / 50
+    rus_rs_fpa_average = sum(rus_rs_fpa_list) / 50
+    rus_dtr_fpa_average = sum(rus_dtr_fpa_list) / 50
+    rus_lr_fpa_average = sum(rus_lr_fpa_list) / 50
+    rus_brr_fpa_average = sum(rus_brr_fpa_list) / 50
+
+    print('csrs_fpa_list :', csrs_fpa_list)
+    print('irsvm_fpa_average :', irsvm_fpa_average)
+    print('ltr_fpa_average :', ltr_fpa_average)
+    print('rs_fpa_average :', rs_fpa_average)
+    print('dtr_fpa_average :', dtr_fpa_average)
+    print('lr_fpa_average :', lr_fpa_average)
+    print('brr_fpa_average :', brr_fpa_average)
+    print('rus_fpa_average :', rus_ltr_fpa_average)
+    print('rus_rs_fpa_average :', rus_rs_fpa_average)
+    print('rus_dtr_fpa_average :', rus_dtr_fpa_average)
+    print('rus_lr_fpa_average :', rus_lr_fpa_average)
+    print('rus_brr_fpa_average :', rus_brr_fpa_average)
