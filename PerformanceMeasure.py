726fe75fb32e833bd519bfbcaf923105ab4510cd
frankstyle520 <frankstyle520@users.noreply.github.com>
2018-10-13 13:08:03 +0800

    Update PerformanceMeasure.py
    add norm(popt)

--- PerformanceMeasure.py
+++ PerformanceMeasure.py
@@ -177,7 +177,145 @@ class PerformanceMeasure():
 
         return helper(num_real=num_real)
 
+        def OPT1(self,codeN):
+        codeN_sum = np.sum(codeN)
+        pred_sum = np.sum(self.pred)
+        real_sum = np.sum(self.real)
+        
+        if pred_sum == 0:
+            pred_sum = 1
+            
+        if codeN_sum == 0:
+            codeN_sum = 1
 
+        optimal_index = [j/i if j!=0 and i!=0 else 0 for i , j in zip(codeN,self.real)]
+        optimal_index = list(np.argsort(optimal_index))
+        optimal_index.reverse()
+
+        pred_index = [j / i if j!=0 and i!=0 else 0 for i, j in zip(codeN, self.pred)]
+        pred_index = list(np.argsort(pred_index))
+        pred_index.reverse()
+
+        optimal_X = [0]
+        optimal_Y = [0]
+        for i in optimal_index:
+            optimal_X.append(codeN[i]/codeN_sum + optimal_X[-1])
+            optimal_Y.append(self.real[i]/real_sum + optimal_Y[-1])
+
+        optimal_auc = 0.
+        prev_x = 0
+        prev_y = 0
+        for x, y in zip(optimal_X,optimal_Y):
+            if x != prev_x:
+                optimal_auc += (x - prev_x) * (y + prev_y)/2.
+                prev_x = x
+                prev_y = y
+
+        pred_X = [0]
+        pred_Y = [0]
+        for i in pred_index:
+            pred_X.append(codeN[i]/codeN_sum + pred_X[-1])
+            pred_Y.append(self.real[i]/real_sum + pred_Y[-1])
+
+        pred_auc = 0.
+        prev_x = 0
+        prev_y = 0
+        for x, y in zip(pred_X, pred_Y):
+            if x != prev_x:
+                pred_auc += (x - prev_x) * (y + prev_y)/2.
+                prev_x = x
+                prev_y = y
+
+        optimal_index.reverse()
+        mini_X = [0]
+        mini_Y = [0]
+        for i in optimal_index:
+            mini_X.append(codeN[i] / codeN_sum + mini_X[-1])
+            mini_Y.append(self.real[i] / real_sum + mini_Y[-1])
+            print("({},{})".format(mini_X[-1], mini_Y[-1]), end="")
+        print()
+
+        mini_auc = 0.
+        prev_x = 0
+        prev_y = 0
+        for x, y in zip(mini_X, mini_Y):
+            if x != prev_x:
+                mini_auc += (x - prev_x) * (y + prev_y) / 2.
+                prev_x = x
+                prev_y = y
+        mini_auc = 1 - (optimal_auc -mini_auc )
+        normOPT = ((1-(optimal_auc - pred_auc)) - mini_auc)/(1 - mini_auc)
+        return normOPT
+
+    def OPT(self, codeN):
+        '''
+        有四个模块m1,m2,m3,m4，真实缺陷个数分别为5，2，1，0,self.real=[5，2，1，0],
+        代码行数为10，2，100，50，真实的缺陷密度为[0.5,1,0.01,0]
+        预测m1缺陷个数为1，m2缺陷个数为0，m3缺陷个数为1，m4缺陷个数为50,self.pred=[1,0,1,50],
+        预测的缺陷密度为[0.1,0,0.01,1]
+        optimal model’s curve (0,0),（2/162,0.25）,(12/162,0.875),(112/162,1),(1,1)
+        worst model’s curve (0,0),(50/162,0,(150/162,0.125),(160/162,0.75),(1,1)
+        prediction model’s curve (0,0),(50/162,0),(60/162,0.625),(160/162,0.75),(1,1)
+        from sklearn import metrics
+        optimalx = np.array([0,2/162, 12/162, 112/162, 1])
+        optimaly = np.array([0,0.25, 0.875, 1, 1])
+        optimalauc=metrics.auc(optimalx, optimaly)
+        worsetx = np.array([0,50/162, 150/162, 160/162, 1])
+        worsety = np.array([0,0.0, 0.125, 0.75, 1])
+        worsetauc=metrics.auc(worsetx, worsety)
+        predx = np.array([0,50/162, 60/162, 160/162, 1])
+        predy = np.array([0,0.0, 0.625, 0.75, 1])
+        predauc=metrics.auc(predx, predy)
+        popt=1-(optimalauc-predauc)
+        minpopt=1-(optimalauc-worsetauc)
+        normpopt=(popt-minpopt)/(1-minpopt)
+        print (normpopt)
+        输出得 0.446265938069
+        :param codeN: 代码行数
+        :return: Norm(opt)
+        '''
+        codeN_sum = np.sum(codeN)
+        real_sum = np.sum(self.real)
+
+        optimal_index = [j / i if j != 0 and i != 0 else 0 for i, j in zip(codeN, self.real)]
+        optimal_index = list(np.argsort(optimal_index))
+        optimal_index.reverse()
+
+        pred_index = [j / i if j != 0 and i != 0 else 0 for i, j in zip(codeN, self.pred)]
+        pred_index = list(np.argsort(pred_index))
+        pred_index.reverse()
+
+        optimal_X = [0]
+        optimal_Y = [0]
+        for i in optimal_index:
+            optimal_X.append(codeN[i] / codeN_sum + optimal_X[-1])
+            optimal_Y.append(self.real[i] / real_sum + optimal_Y[-1])
+
+        pred_X = [0]
+        pred_Y = [0]
+        for i in pred_index:
+            pred_X.append(codeN[i] / codeN_sum + pred_X[-1])
+            pred_Y.append(self.real[i] / real_sum + pred_Y[-1])
+
+        optimal_index.reverse()
+        mini_X = [0]
+        mini_Y = [0]
+        for i in optimal_index:
+            mini_X.append(codeN[i] / codeN_sum + mini_X[-1])
+            mini_Y.append(self.real[i] / real_sum + mini_Y[-1])
+
+        a = [i-j for i,j in zip(optimal_X , [2 / 162, 12 / 162, 112 / 162, 1])]
+        b = [i - j for i, j  in zip(optimal_Y , [0.25, 0.875, 1, 1])]
+
+        worse_auc = metrics.auc(list(mini_X), list(mini_Y))
+        optimal_auc = metrics.auc(list(optimal_X), list(optimal_Y))
+        pred_auc = metrics.auc(list(pred_X), list(pred_Y))
+        dotaopt = optimal_auc - pred_auc
+        miniopt = 1 - (optimal_auc - worse_auc)
+        popt = 1 - dotaopt
+        normOPT = (popt - miniopt) / (1 - miniopt)
+
+        return normOPT
 
 
     def calc_UN(self, type):
